6dcae4e5e60b1a6f4bf496b8301cadf54ca60dfa
spr-tech <adetoroshina434@gmail.com>
2026-09-23 11:42:09 +0100

    Build login and get-started pages with Formik

--- src/pages/GetStarted.tsx
+++ src/pages/GetStarted.tsx
@@ -1,10 +1,89 @@
+import { useFormik } from "formik";
+import { Link } from "react-router-dom";
+import AuthLayout from "../components/layout/AuthLayout";
+import FormInput from "../components/ui/FormInput";
+import Button from "../components/ui/Button";
+import { signupSchema } from "../validation/schemas";
 
 const GetStarted = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: signupSchema,
+    onSubmit: async (values, { resetForm }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      console.log(values);
+      resetForm();
+    },
+  });
 
-export default GetStarted
+  return (
+    <AuthLayout
+      title="Get Started"
+      subtitle="Create your secure Rubies account"
+      footer={
+        <>
+          Already have an account?{" "}
+          <Link
+            to="/login"
+            className="text-neutral-900 font-medium hover:underline"
+          >
+            Login
+          </Link>
+        </>
+      }
+    >
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-5">
+        <FormInput
+          label="First Name"
+          placeholder="Enter your first name"
+          error={formik.errors.firstName}
+          touched={formik.touched.firstName}
+          {...formik.getFieldProps("firstName")}
+        />
+
+        <FormInput
+          label="Last Name"
+          placeholder="Enter your last name"
+          error={formik.errors.lastName}
+          touched={formik.touched.lastName}
+          {...formik.getFieldProps("lastName")}
+        />
+
+        <FormInput
+          label="Work email"
+          type="email"
+          placeholder="you@company.com"
+          error={formik.errors.email}
+          touched={formik.touched.email}
+          {...formik.getFieldProps("email")}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          placeholder="Create a password"
+          error={formik.errors.password}
+          touched={formik.touched.password}
+          {...formik.getFieldProps("password")}
+        />
+
+        <Button
+          type="submit"
+          loading={formik.isSubmitting}
+          loadingText="Creating account..."
+          disabled={formik.isSubmitting || !formik.dirty}
+          className="cursor-pointer"
+        >
+          Continue
+        </Button>
+      </form>
+    </AuthLayout>
+  );
+};
+
+export default GetStarted;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,77 @@
+import { useFormik } from "formik";
+import { Link } from "react-router-dom";
+import AuthLayout from "../components/layout/AuthLayout";
+import FormInput from "../components/ui/FormInput";
+import Button from "../components/ui/Button";
+import { loginSchema } from "../validation/schemas";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema: loginSchema,
+    onSubmit: async (values, { resetForm }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      console.log(values);
+      resetForm();
+    },
+  });
 
-export default Login
+  return (
+    <AuthLayout
+      title="Welcome Back"
+      subtitle="Sign in "
+      footer={
+        <>
+          Don't have an account?{" "}
+          <Link
+            to="/get-started"
+            className="text-neutral-900 font-medium hover:underline"
+          >
+            Get Started
+          </Link>
+        </>
+      }
+    >
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-5">
+        <FormInput
+          label="Email"
+          type="email"
+          placeholder="you@company.com"
+          error={formik.errors.email}
+          touched={formik.touched.email}
+          {...formik.getFieldProps("email")}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          error={formik.errors.password}
+          touched={formik.touched.password}
+          {...formik.getFieldProps("password")}
+        />
+
+        <div className="text-right -mt-1">
+          <Link
+            to="/forgot-password"
+            className="text-sm text-neutral-500 hover:text-neutral-900"
+          >
+            Forgot your password?
+          </Link>
+        </div>
+
+        <Button
+          type="submit"
+          loading={formik.isSubmitting}
+          loadingText="Signing in..."
+          disabled={formik.isSubmitting || !formik.dirty}
+          className="cursor-pointer"
+        >
+          Sign in
+        </Button>
+      </form>
+    </AuthLayout>
+  );
+};
+
+export default Login;
